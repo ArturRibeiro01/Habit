76ecd8644020ed2777d706630966a23a8e00e7a1
Artur Ribeiro <profissional.arturrc@gmail.com>
2022-08-01 12:53:15 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Artur Mac on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
